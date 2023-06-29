05887a3b95ef5e734572fafad3f6f79a0b4a947d
sapzilking <sapzilking@gmail.com>
2023-06-29 23:25:24 +0900

    동시 세션 제어 실습

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,65 +2,29 @@ package io.security.basicsecurity;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import javax.servlet.http.HttpSession;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     Logger LOG = LoggerFactory.getLogger(SecurityConfig.class);
 
-    @Autowired
-    UserDetailsService userDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
+        http
+                .formLogin();
 
         http
-                .formLogin()
-//                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
-                .successHandler((request, response, authentication) -> {
-                    LOG.info("authentication name: {}", authentication.getName());
-                    response.sendRedirect("/");
-                })
-                .failureHandler((request, response, exception) -> {
-                    LOG.info("exception: {}", exception.getMessage());
-                    response.sendRedirect("login");
-                })
-                .permitAll();
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true);
 
-        http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler((request, response, authentication) -> {
-                    HttpSession session = request.getSession();
-                    session.invalidate();
-                })
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    response.sendRedirect("/login");
-                })
-                .deleteCookies("remember-me")
-                .and()
-                .rememberMe()
-                .rememberMeParameter("remember")
-                .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService)
-        ;
     }
 
 }
